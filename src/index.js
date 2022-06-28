483a7ebdd36362b610cf85fd601fc4ab82837117
James Chemobo <jkchemobo@gmail.com>
2022-06-28 12:51:43 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded',()=>{
+  const card = document.querySelector('form').addEventListener('submit',(event)=>{
+    event.preventDefault();
+
+    let form = document.getElementById('new-task-description').value;
+    let list = document.createElement('li');
+    let button = document.createElement('button');
+    button.addEventListener('click',(event)=>{
+    event.target.parentNode.remove();
+    });
+    button.textContent = 'x'
+    list.textContent = `${form}`
+    list.appendChild(button);
+    document.querySelector('#tasks').appendChild(list);
+  })
+})
